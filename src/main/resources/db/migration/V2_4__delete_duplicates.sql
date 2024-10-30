d2bc8c6c5b86574dc2f16b5ae6200b63aac89dd7
Anders Rognstad <anders.rognstad@nav.no>
2024-10-30 09:47:03 +0100

    IS-2757: Delete duplicates

---
+++ src/main/resources/db/migration/V2_4__delete_duplicates.sql
@@ -0,0 +1,7 @@
+delete from sen_oppfolging_kandidat where varsel_id in (
+                                           '1cdae773-3c78-43bd-bf6a-c846ade082ca',
+                                           '3451bb32-a866-4362-a1d4-ae9917ae26f4',
+                                           '81412521-d875-454c-a3ae-46cf8e50b81e',
+                                           '83dcd633-746f-4bfd-ad29-caf19fddc68c',
+                                           'ee5522b3-63de-44fb-a74d-7c2e22860bee',
+                                           '59f96ed4-fe6f-4c94-a84d-faed6ff90a61');
