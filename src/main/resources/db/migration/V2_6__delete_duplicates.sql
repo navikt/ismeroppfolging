946a85dcb47be8c14f0923030babc40dab0ca07f
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-10-31 13:19:38 +0100

    IS-2757: delete duplicates (#50)

---
+++ src/main/resources/db/migration/V2_6__delete_duplicates.sql
@@ -0,0 +1,2 @@
+delete from sen_oppfolging_kandidat where varsel_id in
+                                          ('dedf0d56-bba1-4d97-984c-3360b1fa195a','0c68f559-69c3-493f-a148-b8e464be8ffc','f124acc7-897a-4ace-8be5-81726d24b413');
