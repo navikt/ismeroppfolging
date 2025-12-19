6781e4d758a76b75f327d7e3914431086471da38
Eirik Dahlen <37441744+eirikdahlen@users.noreply.github.com>
2025-12-19 15:26:34 +0100

    IS-3611: Add pilotkontorer for processing (#201)

--- src/test/kotlin/no/nav/syfo/kartleggingssporsmal/infrastructure/mock/BehandlendeenhetMock.kt
+++ src/test/kotlin/no/nav/syfo/kartleggingssporsmal/infrastructure/mock/BehandlendeenhetMock.kt
@@ -5,8 +5,10 @@ import io.ktor.client.request.*
 import io.ktor.http.HttpStatusCode
 import no.nav.syfo.UserConstants.ARBEIDSTAKER_PERSONIDENT_ANNEN_ENHET
 import no.nav.syfo.UserConstants.ARBEIDSTAKER_PERSONIDENT_INACTIVE
-import no.nav.syfo.shared.infrastructure.mock.respond
+import no.nav.syfo.UserConstants.ARBEIDSTAKER_PERSONIDENT_PILOT_UTEN_UTSENDING
+import no.nav.syfo.UserConstants.VALID_PILOTKONTOR_UTEN_VARSEL
 import no.nav.syfo.kartleggingssporsmal.generators.getBehandlendeEnhetDTO
+import no.nav.syfo.shared.infrastructure.mock.respond
 import no.nav.syfo.shared.util.NAV_PERSONIDENT_HEADER
 
 fun MockRequestHandleScope.behandlendeenhetResponse(request: HttpRequestData): HttpResponseData {
@@ -17,6 +19,9 @@ fun MockRequestHandleScope.behandlendeenhetResponse(request: HttpRequestData): H
         ARBEIDSTAKER_PERSONIDENT_INACTIVE.value -> respond(
             body = emptyList<String>(),
             statusCode = HttpStatusCode.NoContent,
+        )
+        ARBEIDSTAKER_PERSONIDENT_PILOT_UTEN_UTSENDING.value -> respond(
+            getBehandlendeEnhetDTO(geografiskEnhetId = VALID_PILOTKONTOR_UTEN_VARSEL)
         )
         else -> respond(getBehandlendeEnhetDTO())
     }
