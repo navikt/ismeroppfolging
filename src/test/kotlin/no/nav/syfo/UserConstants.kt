7ad37ef75b505f77881235bc7316cdc3eefd7f16
eirikdahlen <dahleneirik@gmail.com>
2026-01-08 15:55:06 +0100

    IS-3624: Add pilotkontorer in Oslo for varsling
    Merges søndag kveld

--- src/test/kotlin/no/nav/syfo/UserConstants.kt
+++ src/test/kotlin/no/nav/syfo/UserConstants.kt
@@ -28,7 +28,7 @@ object UserConstants {
     const val VEILEDER_IDENT = "Z999999"
     const val VIRKSOMHETSNUMMER = "123456789"
     const val VALID_PILOTKONTOR = "0220"
-    const val VALID_PILOTKONTOR_UTEN_VARSEL = "0318"
+    const val VALID_PILOTKONTOR_UTEN_VARSEL = "0710"
     const val PERSON_FORNAVN = "Fornavn"
     const val PERSON_MELLOMNAVN = "Mellomnavn"
     const val PERSON_ETTERNAVN = "Etternavnesen"
